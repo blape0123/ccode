0e099aae7667e321551ff6cb72e2fe8a479348b3
blape0123 <blape0123@gmail.com>
2026-06-06 01:05:08 +0000

    5일차

--- day4/homework.c
+++ day4/homework.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 
+int power(int b) {
+    if(b == 0) {
+        return 1;
+    }
+    return 2 * power(b - 1);
+}
+
+int count = 0;
+
 void printBinary(int n)
 {
-    n = 0;
+    while (power(count) <= n) {
+        count++;
+    }
+
+    count--;
+
+    if (n <= 0) {
+        count = 0;
+        return;
+    }
+    else if(n >= power(count)) {
+        printf("1");
+        count--;
+        printBinary(n - power(count));
+    }
+    else {
+        printf("0");
+        count--;
+        printBinary(n - power(count));
+    }
 }
 
 int main()
