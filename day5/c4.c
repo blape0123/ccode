52f7fde0f3d1142c717047c5aca0029125cc7221
blape0123 <blape0123@gmail.com>
2026-06-06 01:27:33 +0000

    5일차-2

--- day5/c4.c
+++ day5/c4.c
@@ -16,6 +16,11 @@ int mm(int a, int b, int *c, int *d) {
     }
 }
 
+void pv(const int *p) {
+    printf("%d\n", *p);
+    //*p = 100;
+}
+
 int main(void) {
     int q;
     int r;
@@ -26,4 +31,5 @@ int main(void) {
     printf("17 / 5 → 몫 = %d, 나머지 = %d\n", q, r);
     mm(8, 3, &max, &min);
     printf("min = %d, max = %d\n", min, max);
+    pv(&q);
 }
